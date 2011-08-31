4ae5a16af6b943fc8e610dd22b7262276dfd97b5
Peter Donald <peter@realityforge.org>
2011-08-31 12:36:45 +1000

    Update scripts

--- backup-github-accounts.sh
+++ backup-github-accounts.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-export RF_REPOS=`curl --silent  https://api.github.com/users/realityforge/repos | grep git_url | tr ',"' ' ' | awk '{print $3}' | xargs echo`
-export SS_REPOS=`curl --silent  https://api.github.com/users/stocksoftware/repos | grep git_url | tr ',"' ' ' | awk '{print $3}' | xargs echo`
+for ACCOUNT in realityforge stocksoftware; do
 
-export REPO_LIST="$RF_REPOS $SS_REPOS"
+  export ACCOUNT_LOCATION="~/Backups/github/$ACCOUNT"
 
-cd ~/Backups/github
+  export REPO_LIST=`curl --silent  https://api.github.com/users/$ACCOUNT/repos | grep git_url | tr ',"' ' ' | awk '{print $3}' | xargs echo`
 
-for REPO in $REPO_LIST; do
-	echo "Backing up $REPO"
-    rm -rf `basename $REPO`
-    git clone --bare $REPO `basename $REPO`
-    (cd `basename $REPO` && git remote add --mirror origin $REPO)
+  mkdir -p $ACCOUNT_LOCATION
+
+  for REPO in $REPO_LIST; do
+	export BASE_NAME=`basename $REPO .git`
+	export LOCATION="$ACCOUNT_LOCATION/$BASE_NAME"
+	echo "Backing up $ACCOUNT/$BASE_NAME to $LOCATION"
+    rm -rf $LOCATION
+    git clone --bare $REPO $LOCATION
+    (cd $LOCATION && git remote add --mirror origin $REPO)
+  done
+
 done
