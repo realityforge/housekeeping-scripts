1e46a2975b0e17205ecc9038068f1881851059db
Peter Donald <peter@realityforge.org>
2011-08-31 17:04:53 +1000

    Update script

--- smugmug/allsmugget.sh
+++ smugmug/allsmugget.sh
@@ -11,22 +11,26 @@ test -f ~/.smugup && source ~/.smugup
 UA="smugget/1.1 (smugmug@dotd.com)"
 APIKEY="rjBy6Da5lMEbguUkqwpFEdzmOALQrsIE"
 
-while getopts "a:p:u:" flag; do
+while getopts "n:p:u:" flag; do
     case $flag in
+	n)
+	    NICKNAME=$OPTARG
+	    ;;
 	u)
 	    EMAIL=$OPTARG
-	    shift;shift;;
+	    ;;
 	p)
 	    PASSWORD=$OPTARG
-	    shift;shift;;
+	    ;;
 	*)
-	    echo "Usage: $0 [-u email] [-p password] [-a albumId] files..."
+	    echo "Usage: $0 [-u email] [-p password] files..."
 	    exit 1
     esac
 done
 
 test -z "$EMAIL" && echo "Username missing" && exit 1
 test -z "$PASSWORD" && echo "Password missing" && exit 1
+test -z "$NICKNAME" && echo "NickName missing" && exit 1
 
 SID=`curl -k -A "$UA" -s "https://api.smugmug.com/hack/rest/1.1.1/?method=smugmug.login.withPassword&EmailAddress=$EMAIL&Password=$PASSWORD&APIKey=$APIKEY" | grep SessionID`
 SID=${SID/*<SessionID>/}
@@ -34,10 +38,9 @@ SID=${SID/<\/SessionID>*/}
 
 test -z $SID && echo "Unable to login" && exit 1
 
-curl -k -A "$UA" -s "https://api.smugmug.com/hack/rest/1.1.1/?method=smugmug.albums.get&SessionID=$SID&APIKey=$APIKEY"|
-awk -F'[<\"][^>\"]*[>\"]' \
-'BEGIN { ORS="" } /Album / {album = $2; p = 1}; /Title/ {if (p) { print album " " $2 "\n"; p = 0 } }' |
-sort -rn > $0.$$.tmp
+curl -k -A "$UA" -s "http://api.smugmug.com/services/api/rest/1.3.0/?method=smugmug.albums.get&SessionID=$SID&APIKey=$APIKEY&NickName=$NICKNAME"| sed -n '
+s/.*<Album id="\(.*\)">/\1/p
+' | sort -rn > $0.$$.tmp
 
 curl -k -s -o /dev/null -A "$UA" "https://api.smugmug.com/hack/rest/1.1.1/?method=smugmug.logout&SessionID=$SID&APIKey=$APIKEY"
 
