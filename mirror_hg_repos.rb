4963ac7e3152205f44ca9836f5508bd3413b66b2
Peter Donald <peter@realityforge.org>
2012-09-23 20:29:06 +1000

    Ensure that a failed command results in a failure

--- mirror_hg_repos.rb
+++ mirror_hg_repos.rb
@@ -11,21 +11,26 @@ hg_to_clone = {
 
 BASE_DIR=`pwd`.strip
 
+def sh(command)
+  system command
+  raise "Failed to execute command #{command} with exit #{$?}" if $?.to_s != "0"
+end
+
 hg_to_clone.each_pair do |target, source|
   directory = "#{BASE_DIR}/#{File.basename(source)}"
   unless File.directory?(directory)
     puts "Directory #{directory} for repository #{source} does not exist. Cloning..."
     Dir.chdir(BASE_DIR)
-    system "hg clone #{source} #{directory}"
+    sh "hg clone #{source} #{directory}"
     puts "In directory #{directory} make a bookmark of master for default, so a ref gets created."
     Dir.chdir(directory)
-    system "hg bookmark -r default master"
+    sh "hg bookmark -r default master"
   end
   puts "Updating directory #{directory} for repository #{source}."
   Dir.chdir(directory)
-  system "hg pull"
+  sh "hg pull"
 
   target = "git+ssh://git@github.com/#{target}.git"
   puts "Pushing directory #{directory} to git repository #{target}."
-  system "hg push #{target}"
+  sh "hg push #{target}"
 end
